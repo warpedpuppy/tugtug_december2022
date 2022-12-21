694a8f52a7c50d3a0172a9f5ee04d941a685ad92
ted <ted@warpedpuppy.com>
2022-12-21 08:38:13 -0500

    started code clean

--- src/animations/maze-animation.js
+++ src/animations/maze-animation.js
@@ -1,36 +1,24 @@
-import Utils from './utils/utils'
-import Assets from './utils/assetCreation'
-import Tweens from './utils/Tweens'
-import Clock from './supportingClasses/universal/clock'
-import Swim from './supportingClasses/maze'
-import Gears from './supportingClasses/universal/gears'
-import Hero from './supportingClasses/maze/hero'
-import Config from './animations-config'
-import KeyHandler from './supportingClasses/universal/keyHandler'
-import Grid from './supportingClasses/grid/gridIndex'
-import Resize from './supportingClasses/maze/resize'
-import MazeServices from '../services/maze-service'
-import DefaultMaze from '../defaults/DefaultMaze'
+import Utils from './utils/utils';
+import Assets from './utils/assetCreation';
+import Tweens from './utils/Tweens';
+import Clock from './supportingClasses/universal/clock';
+import Maze from './supportingClasses/maze';
+import Gears from './supportingClasses/universal/gears';
+import Hero from './supportingClasses/maze/hero';
+import KeyHandler from './supportingClasses/universal/keyHandler';
+import Grid from './supportingClasses/grid/gridIndex';
+import Resize from './supportingClasses/maze/resize';
 
 const MazeAnimation = {
-    activeModeIndex: 0,
-    activeMode: undefined,
-    action: true,
     gears: Gears(),
     clock: Clock(),
     hero: Hero(),
+	maze: Maze(),
+    grid: Grid(),
     utils: Utils,
     loader: Assets.Loader(),
     activeAction: undefined,
-    swim: Swim(),
-    grid: Grid(),
-    dbData: {},
-    storeAction: true,
-    timeOut: undefined,
-    fullStop: false,
     kingCont: Assets.Container(),
-    frame: Assets.Graphics(),
-    kingContBackground: Assets.Graphics(),
     resize: Resize(),
 	loaded: false,
 	activeMaze: undefined,
@@ -40,42 +28,32 @@ const MazeAnimation = {
 	mazeCanvas: undefined,
     init (activeMaze) {
 
-			this.activeMaze = activeMaze;
-			this.utils.root = this
-			this.utils.getWidthAndHeight();
+		this.activeMaze = activeMaze;
+		this.utils.root = this
+		this.utils.getWidthAndHeight();
 
+		let { canvasHeight: height, canvasWidth: width } = this.utils;
+		this.app = Assets.Application({width, height, backgroundAlpha: 1})
+		this.mazeCanvas = document.getElementById('maze-canvas');
+		this.mazeCanvas.width = this.utils.canvasWidth;
+		this.mazeCanvas.height = this.utils.canvasHeight;
+		this.mazeCanvas.appendChild(this.app.view)
+		this.stage = this.app.stage
+		this.stage.addChild(this.kingCont)
 		
-			let { canvasHeight: height, canvasWidth: width } = this.utils;
-			this.app = Assets.Application({width, height, backgroundAlpha: 1})
-			this.mazeCanvas = document.getElementById('maze-canvas');
-			this.mazeCanvas.width = this.utils.canvasWidth;
-			this.mazeCanvas.height = this.utils.canvasHeight;
-			this.mazeCanvas.appendChild(this.app.view)
-			this.stage = this.app.stage
-			this.stage.addChild(this.kingCont)
-		
-
-
-
-
-
 		const load = async () => {
-			this.loaded = true;
 			try {
 				this.assets = await PIXI.Assets.loader.load('/ss/ss.json');
-				if (this.assets) this.buildGame()
+				if (this.assets) {this.loaded = true; this.buildGame(); }
 			} catch (e) {
-				
+				// do something here 
 			}
-			
 		}
-		if (!this.loaded) {load();} else this.resetGame();
-		
-
+		if (!this.loaded) { load(); } else { this.resetGame(); }
     },
     buildGame () {
-		console.log("BUILD GAME", this.activeMaze, this.assets)
-		this.grid.boards = [this.activeMaze];
+		
+	  this.grid.boards = [this.activeMaze];
 
       this.utils.setProperties({
         spritesheet: this.assets,
@@ -87,29 +65,26 @@ const MazeAnimation = {
 
       Assets.init()
 
-      this.gears.init().addToStage()
+      this.gears.init().addToStage();
+      this.clock.init().addToStage();
+      this.grid.init(this.activeMaze);
+      this.hero.init(this.kingCont);
+      this.utils.setHero(this.hero);
+      this.maze.init(this.kingCont);
+      this.keyHandler = KeyHandler;
+      this.activeAction = this.maze.addToStage();
+	  this.keyHandler.init(this);
+     
+      this.startGame();
 
-      this.clock.init().addToStage()
-
-      this.grid.init(this.activeMaze)
-
-      this.hero.init(this.kingCont)
-      this.utils.setHero(this.hero)
-      this.swim.init(this.kingCont)
-      this.keyHandler = KeyHandler
-      this.activeAction = this.swim.addToStage()
-	  this.keyHandler.init(this)
-      window.onresize = this.resize.resizeHandler.bind(this.resize)
-      this.startGame()
+	  window.onresize = this.resize.resizeHandler.bind(this.resize)
     },
 	resetGame () {
-
 		this.grid.boards = [this.activeMaze];
-
 		this.stage.addChild(this.kingCont)
 		this.gears.addToStage();
 		this.clock.init().addToStage();
-		this.activeAction = this.swim.addToStage()
+		this.activeAction = this.maze.addToStage()
 		this.keyHandler.init(this);
 		this.hero.init(this.kingCont)
 		this.utils.setHero(this.hero)
@@ -117,33 +92,22 @@ const MazeAnimation = {
 		this.startGame()
 	},
     startGame () {
-   
 		this.app.ticker.add(this.animate)
 		this.keyHandler.addToStage()
 		this.hero.addToStage();
     },
     stop () {
-      window.onresize = undefined
-	  this.stage.removeChildren();
-	  this.kingCont.removeChildren();
-	  this.gears.removeFromStage();
-	  this.hero.removeFromStage();
-		this.swim.removeFromStage();
-	this.grid.removeFromStage();
-	this.keyHandler.removeFromStage();
-      this.clock.removeFromStage()
-      this.app.destroy();
-      Tweens.killAll()
-    },
-    reset () {
-
-
-      // this[this.activeMode].removeFromStage();
-
-      this.grid.nextBoard()
-      this.keyHandler.addToStage()
-
-      this.fullStop = false
+		window.onresize = undefined
+		this.stage.removeChildren();
+		this.kingCont.removeChildren();
+		this.gears.removeFromStage();
+		this.hero.removeFromStage();
+		this.maze.removeFromStage();
+		this.grid.removeFromStage();
+		this.keyHandler.removeFromStage();
+		this.clock.removeFromStage()
+		this.app.destroy();
+		Tweens.killAll()
     },
     animate () {
 		Tweens.animate()
@@ -154,7 +118,7 @@ const MazeAnimation = {
 		}
 		MazeAnimation.clock.animate()
 		MazeAnimation.gears.animate()
-		MazeAnimation.swim.animate();
+		MazeAnimation.maze.animate();
 		MazeAnimation.grid.animate(MazeAnimation.activeAction.vx, MazeAnimation.activeAction.vy)
     }
 }
